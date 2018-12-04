5922963a593fa7e24fdae86de5e946801d4b3df2
Quan Hui Wang <quanhui.wang@d2l.com>
2018-12-04 15:30:09 -0500

    Convert search-header to polymer 3. Add d2l-typography to main body. Fix 404 page routing. Fix a lint warning for route-locations-mixin by adding @polymerMixin annotation.

---
+++ src/components/search-header.js
@@ -0,0 +1,143 @@
+'use strict';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import 'd2l-icons/d2l-icons.js';
+import 'd2l-icons/tier2-icons.js';
+import { RouteLocationsMixin } from '../mixins/route-locations-mixin.js';
+import { LocalizeMixin } from '../mixins/localize-mixin.js';
+
+class SearchHeader extends RouteLocationsMixin(LocalizeMixin(PolymerElement)) {
+	static get template() {
+		return html`
+			<style>
+				:host {
+					display: inline;
+				}
+
+				.container {
+					overflow: auto;
+					background-color: white;
+					display: flex;
+					flex-direction: row;
+					flex-wrap: nowrap;
+					align-items: center;
+					border-bottom: 1px solid #f2f3f5;
+				}
+
+				.content {
+					flex-shrink: 0;
+					padding-left: 1rem;
+				}
+
+				.box {
+					-webkit-box-shadow: 0 1px 4px rgba(86,86,86,0.2);
+					-moz-box-shadow: 0 1px 4px rgba(86,86,86,0.2);
+					box-shadow: 0 1px 4px rgba(86,86,86,0.2);
+				}
+
+				.clickable {
+					cursor: pointer;
+				}
+
+				.searchBarContainer {
+					padding-left: 1rem;
+					width: 100%;
+				}
+				.searchBar{
+					width: 50%;
+					min-width: 100px;
+				}
+
+				.endContent {
+					flex-shrink: 0;
+					padding-left: 3rem;
+					padding-right: 3rem;
+				}
+			</style>
+
+			<div class="box">
+				<div class="container">
+					<div class="content">
+						<h2 class="d2l-heading-2 clickable" on-click="_navigateToHome">
+							[[localize('discover')]]
+						</h2>
+					</div>
+
+					<div class="content">
+						<d2l-icon icon="d2l-tier2:divider"></d2l-icon>
+					</div>
+
+					<div class="content">
+						<span class="d2l-body-standard">[[localize('searchLabel')]]</span>
+					</div>
+
+					<div class="searchBarContainer">
+						<!-- This is a placeholder search inputbox until d2l-inputs is converted to Polymer 3 -->
+						<input
+							id="search-input"
+							class="searchBar"
+							type="text"
+							placeholder="[[localize('searchPlaceholder')]]"
+							value="[[query]]">
+					</div>
+
+					<div class="endContent">
+					</div>
+				</div>
+			</div>
+		`;
+	}
+	constructor() {
+		super();
+		this.query = '';
+	}
+	static get properties() {
+		return {
+			query: {
+				type: String,
+				notify: true,
+				observer: '_queryChanged',
+			}
+		};
+	}
+	ready() {
+		super.ready();
+		const searchInput = this.shadowRoot.querySelector('#search-input');
+		if (searchInput) {
+			searchInput.addEventListener('keyup', (e) => {
+				e.preventDefault();
+				if (e.keyCode === 13) { // Enter key
+					this.query = searchInput.value;
+				}
+			});
+		}
+	}
+	clear() {
+		this.query = '';
+		const searchInput = this.shadowRoot.querySelector('#search-input');
+		if (searchInput) {
+			searchInput.value = '';
+		}
+	}
+	_navigateToHome() {
+		this.dispatchEvent(new CustomEvent('navigate', {
+			detail: {
+				path: this.routeLocations().navLink(),
+			},
+			bubbles: true,
+			composed: true,
+		}));
+	}
+	_queryChanged(query) {
+		if (query) {
+			this.dispatchEvent(new CustomEvent('navigate', {
+				detail: {
+					path: this.routeLocations().search(query.trim())
+				},
+				bubbles: true,
+				composed: true,
+			}));
+		}
+	}
+}
+
+window.customElements.define('search-header', SearchHeader);
