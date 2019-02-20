38f4424c9cbbf7d29bc899000d39f10289435214
Quan Hui Wang <quanhui.wang@d2l.com>
2019-02-20 13:44:52 -0500

    initial dev work for adding enrollment workflow

--- src/components/course-summary.js
+++ src/components/course-summary.js
@@ -2,7 +2,6 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import { IronResizableBehavior } from '@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
 import '@polymer/paper-dialog/paper-dialog.js';
-import 'd2l-alert/d2l-alert-toast.js';
 import 'd2l-colors/d2l-colors.js';
 import 'd2l-button/d2l-button.js';
 import 'd2l-icons/d2l-icon.js';
@@ -11,10 +10,11 @@ import 'd2l-link/d2l-link.js';
 import 'd2l-typography/d2l-typography.js';
 import 'fastdom/fastdom.js';
 
+import { FetchMixin } from '../mixins/fetch-mixin.js';
 import { LocalizeMixin } from '../mixins/localize-mixin.js';
 import { RouteLocationsMixin } from '../mixins/route-locations-mixin.js';
 
-class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixin(RouteLocationsMixin(PolymerElement))) {
+class CourseSummary extends mixinBehaviors([IronResizableBehavior], FetchMixin(LocalizeMixin(RouteLocationsMixin(PolymerElement)))) {
 	static get template() {
 		return html `
 			<style include="d2l-typography">
@@ -88,29 +88,6 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 					padding: 1.5rem;
 				}
 
-				.discovery-course-summary-dialog-button-container {
-					display: flex;
-					flex-direction: row;
-					margin-top: 2rem;
-					overflow: auto;
-				}
-
-				.discovery-course-summary-alert {
-					height: auto;
-				}
-
-				.discovery-course-summary-alert-container {
-					display: flex;
-					flex-direction: row;
-					justify-content: space-between;
-					overflow: auto;
-				}
-
-				.discovery-course-summary-alert-content {
-					margin-left: 0.5rem;
-					margin-right: 0.5rem;
-				}
-
 				.discovery-course-summary-dialog {
 					border-radius: 5px;
 					overflow: auto;
@@ -123,18 +100,18 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 				}
 
 				.discovery-course-summary-dialog-header-container {
-					display: flex;
-					flex-direction: row;
-					justify-content: space-between;
+					margin-bottom: 0.5rem;
+					text-align: right;
 				}
 
 				.discovery-course-summary-dialog-close {
 					cursor: pointer;
+					float: right;
 					font-size: 1.4rem;
 				}
 
-				.discovery-course-summary-second-dialog-button {
-					margin-left: 1rem;
+				.discovery-course-summary-dialog-content-container{
+					margin-right: 1rem;
 				}
 
 				.discovery-course-summary-d2l-heading-1 {
@@ -250,22 +227,11 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 				</div>
 
 				<div id="discovery-course-summary-buttons" class="discovery-course-summary-buttons">
-					<template is="dom-if" if="[[isInMyLearning]]">
-						<d2l-button on-click="_addToMyLearning" primary>[[localize('accessMaterials')]]</d2l-button>
+					<template is="dom-if" if="[[!actionEnroll]]">
+						<d2l-button on-click="_navigateToActivityHomepage" primary>[[localize('openCourse')]]</d2l-button>
 					</template>
-					<template is="dom-if" if="[[!isInMyLearning]]">
-						<d2l-button on-click="_addToMyLearning" primary>[[localize('enrollInCourse')]]</d2l-button>
-					</template>
-					<template is="dom-if" if="[[isInMyLearning]]">
-						<d2l-button on-click="_addToMyList">[[localize('unenroll')]]</d2l-button>
-					</template>
-					<template is="dom-if" if="[[!isInMyLearning]]">
-						<template is="dom-if" if="[[isOnMyList]]">
-							<d2l-button on-click="_addToMyList">[[localize('onMyList')]]</d2l-button>
-						</template>
-						<template is="dom-if" if="[[!isOnMyList]]">
-							<d2l-button on-click="_addToMyList">[[localize('addToMyList')]]</d2l-button>
-						</template>
+					<template is="dom-if" if="[[actionEnroll]]">
+						<d2l-button on-click="_enroll" primary>[[localize('enrollInCourse')]]</d2l-button>
 					</template>
 				</div>
 
@@ -275,32 +241,13 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 				</div>
 			</div>
 
-			<d2l-alert-toast class="discovery-course-summary-alert" id="addedAlert" type="success" hide-close-button>
-				<div class="discovery-course-summary-alert-container">
-					<div class="discovery-course-summary-alert-content">[[localize('addedToYourList')]]</div>
-					<div class="discovery-course-summary-alert-content">
-						<d2l-link href="javascript:void(0)" on-click="_navigateToMyList">[[localize('viewMyList')]]</d2l-link>
-					</div>
-				</div>
-			</d2l-alert-toast>
-
 			<paper-dialog class="discovery-course-summary-dialog d2l-typography" id="myLearningDialog" always-on-top with-backdrop>
 				<div class="discovery-course-summary-dialog-container">
 					<div class="discovery-course-summary-dialog-header-container">
-						<h4 class="d2l-heading-4 discovery-course-summary-d2l-heading-4">[[localize('addedToMyLearningHeader')]]</h4>
 						<d2l-icon class="discovery-course-summary-dialog-close" on-click="_closeDialog" icon="d2l-tier1:close-small"></d2l-icon>
 					</div>
-
-					<h2 class="d2l-heading-2 discovery-course-summary-d2l-heading-2">[[courseTitle]]</h2>
-					<h4 class="d2l-heading-4 discovery-course-summary-d2l-heading-4">[[localize('welcomeToTheCourse')]]</h4>
-					<div class="d2l-body-standard">[[localize('addedToMyLearningMessage')]]</div>
-					<div class="discovery-course-summary-dialog-button-container">
-						<div>
-							<d2l-button on-click="_navigateToHome" primary>[[localize('startLearning')]]</d2l-button>
-						</div>
-						<div class="discovery-course-summary-second-dialog-button">
-							<d2l-button on-click="_closeDialog">[[localize('continueBrowsing')]]</d2l-button>
-						</div>
+					<div class="discovery-course-summary-dialog-content-container">
+						<div class="d2l-body-standard">[[localize('enrollmentMessage.success')]]</div>
 					</div>
 				</div>
 			</paper-dialog>
@@ -316,14 +263,10 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 			courseLastUpdated: String,
 			courseImage: String,
 			format: String,
-			isInMyLearning: {
-				type: Boolean,
-				notify: true
+			actionEnroll: {
+				type: String
 			},
-			isOnMyList: {
-				type: Boolean,
-				notify: true
-			},
+			activityHomepage: String
 		};
 	}
 
@@ -361,34 +304,7 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 		}
 	}
 
-	_addToMyLearning() {
-		this.isInMyLearning = !this.isInMyLearning; // temporary toggle functionality for testing
-
-		var myLearningDialog = this.shadowRoot.querySelector('#myLearningDialog');
-		myLearningDialog.opened = true;
-	}
-
-	_addToMyList() {
-		this.isOnMyList = !this.isOnMyList; // temporary toggle functionality for testing
-
-		var addedAlert = this.shadowRoot.querySelector('#addedAlert');
-		addedAlert.open = true;
-	}
-
 	_closeDialog() {
-		var myLearningDialog = this.shadowRoot.querySelector('#myLearningDialog');
-		myLearningDialog.opened = false;
-	}
-
-	_navigateToMyList() {
-		this.dispatchEvent(new CustomEvent('navigate', {
-			detail: {
-				path: this.routeLocations().myList(),
-			},
-			bubbles: true,
-			composed: true,
-		}));
-
 		var myLearningDialog = this.shadowRoot.querySelector('#myLearningDialog');
 		myLearningDialog.opened = false;
 	}
@@ -396,10 +312,10 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 	_navigateToHome() {
 		this.dispatchEvent(new CustomEvent('navigate', {
 			detail: {
-				path: this.routeLocations().home(),
+				path: this.routeLocations().home()
 			},
 			bubbles: true,
-			composed: true,
+			composed: true
 		}));
 
 		var myLearningDialog = this.shadowRoot.querySelector('#myLearningDialog');
@@ -410,11 +326,40 @@ class CourseSummary extends mixinBehaviors([IronResizableBehavior], LocalizeMixi
 		if (e && e.target && e.target.value) {
 			this.dispatchEvent(new CustomEvent('navigate', {
 				detail: {
-					path: this.routeLocations().search(e.target.value),
+					path: this.routeLocations().search(e.target.value)
 				},
 				bubbles: true,
-				composed: true,
+				composed: true
 			}));
+		}
+	}
+
+	_navigateToActivityHomepage() {
+		if (this.activityHomepage) {
+			this.dispatchEvent(new CustomEvent('navigate-parent', {
+				detail: {
+					href: this.activityHomepage
+				},
+				bubbles: true,
+				composed: true
+			}));
+		}
+	}
+
+	_enroll() {
+		if (this.actionEnroll) {
+			this._fetchEntity(this.actionEnroll.href, this.actionEnroll.method)
+				.then(() => {
+					// enrollment succeeded
+					this.actionEnroll = '';
+					// Show dialog
+					var myLearningDialog = this.shadowRoot.querySelector('#myLearningDialog');
+					myLearningDialog.opened = true;
+					this.actionEnroll = '';
+				})
+				.catch(() => {
+					// enrollment failed
+				});
 		}
 	}
 }
--- src/discovery-course.js
+++ src/discovery-course.js
@@ -6,7 +6,6 @@ import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import 'd2l-organization-hm-behavior/d2l-organization-hm-behavior.js';
 import 'd2l-colors/d2l-colors.js';
 
-import { IfrauMixin } from './mixins/ifrau-mixin.js';
 import { LocalizeMixin } from './mixins/localize-mixin.js';
 import { RouteLocationsMixin } from './mixins/route-locations-mixin.js';
 import { FetchMixin } from './mixins/fetch-mixin.js';
@@ -17,7 +16,7 @@ import './styles/discovery-styles.js';
 
 class DiscoveryCourse extends mixinBehaviors(
 	[D2L.PolymerBehaviors.Hypermedia.OrganizationHMBehavior],
-	FetchMixin(RouteLocationsMixin(LocalizeMixin(IfrauMixin(PolymerElement))))) {
+	FetchMixin(RouteLocationsMixin(LocalizeMixin(PolymerElement)))) {
 	static get template() {
 		/* global moment*/
 		return html `
@@ -125,8 +124,8 @@ class DiscoveryCourse extends mixinBehaviors(
 					course-duration=[[_courseDuration]]
 					course-last-updated=[[_courseLastUpdated]]
 					format=[[_format]]
-					is-in-my-learning=[[_isInMyLearning]]
-					is-on-my-list=[[_isOnMyList]]>
+					action-enroll=[[_actionEnroll]]
+					activity-homepage=[[_activityHomepage]]>
 				</course-summary>
 
 				<course-action
@@ -144,6 +143,11 @@ class DiscoveryCourse extends mixinBehaviors(
 			route: Object,
 			routeData: Object,
 
+			_actionEnroll: {
+				type: String,
+				value: ''
+			},
+			_activityHomepage: String,
 			_courseCategory: String,
 			_courseCode: String,
 			_courseDescription: String,
@@ -155,8 +159,6 @@ class DiscoveryCourse extends mixinBehaviors(
 			_endDate: String,
 			_firstPublished: String,
 			_format: String,
-			_isInMyLearning: Boolean,
-			_isOnMyList: Boolean,
 			_startDate: String,
 			_courseDescriptionItems: Array
 		};
@@ -184,22 +186,32 @@ class DiscoveryCourse extends mixinBehaviors(
 		}
 	}
 	_handleCourseEntity(courseEntity) {
-		if (!courseEntity.properties) { return; }
+		if (!courseEntity) return Promise.resolve();
 
-		//TODO: These properties still need to be added
-		// 	// data for the course summary
-		// 	this._courseCategory = '';
-		// 	this._courseDuration = null;
-		// 	this._courseLastUpdated = '';
-		// 	this._format = '';
+		if (courseEntity.hasAction('assign') && !courseEntity.hasClass('enroll')) {
+			this._actionEnroll = courseEntity.getAction('assign');
+		}
 
-		// 	this._isInMyLearning = false;
-		// 	this._isOnMyList = false;
+		if (courseEntity.hasLink(Rels.Activities.activityHomepage)) {
+			this._activityHomepage = courseEntity.getLinkByRel(Rels.Activities.activityHomepage).href;
+		}
 
-		// 	// data for course action
-		// 	this._courseTags = [];
-		// 	this._firstPublished = '';
-		this._courseDescription = courseEntity.properties.description;
+		if (courseEntity.properties) {
+			//TODO: These properties still need to be added
+			// 	// data for the course summary
+			// 	this._courseCategory = '';
+			// 	this._courseDuration = null;
+			// 	this._courseLastUpdated = '';
+			// 	this._format = '';
+
+			// 	this._isInMyLearning = false;
+			// 	this._isOnMyList = false;
+
+			// 	// data for course action
+			// 	this._courseTags = [];
+			// 	this._firstPublished = '';
+			this._courseDescription = courseEntity.properties.description;
+		}
 
 		const organizationUrl = courseEntity.hasLink(Rels.organization)
 			&& courseEntity.getLinkByRel(Rels.organization).href;
@@ -271,6 +283,8 @@ class DiscoveryCourse extends mixinBehaviors(
 		this._courseDescriptionItems = courseDescriptionItemsArray;
 	}
 	_reset() {
+		this._actionEnroll = '';
+		this._activityHomepage = '';
 		this._courseCategory = '';
 		this._courseCode = '';
 		this._courseDescription = '';
@@ -282,8 +296,6 @@ class DiscoveryCourse extends mixinBehaviors(
 		this._endDate =  '';
 		this._firstPublished =  '';
 		this._format =  '';
-		this._isInMyLearning =  false;
-		this._isOnMyList =  false;
 		this._startDate =  '';
 		this._courseDescriptionItems = [];
 	}
